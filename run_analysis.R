9a40763f1b580e08098b53a1f0a33fd49ba0c787
Bodil Biering <bodil.biering@gmail.com>
2014-09-22 21:55:13 +0200

    minor correction

--- run_analysis.R
+++ run_analysis.R
@@ -39,7 +39,7 @@ res <- rbind(testres, trainres)
 meanStdNames <- grep("Mean|mean|Std|std", names(res))
 res2 <- res[,c(1,2,meanStdNames)]
 #3. Uses descriptive activity names to name the activities in the data set
-activities <- sapply(res2$activity, function(x) activityLabels[x,"label"])
+res2$activity <- sapply(res2$activity, function(x) activityLabels[x,"label"])
 #4 clean up labels, i.e., lowercase, no dots or parenthesis.
 names(res2) <- gsub("[.]|[(]|[)]", "", names(res2)) %>% 
                tolower
